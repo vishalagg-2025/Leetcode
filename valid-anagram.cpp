0cc8bf7fa1be127dfc33df4e3d19949587f2c3eb
Vishal Agarwal <121810961+vishalagg-2025@users.noreply.github.com>
2024-10-27 22:00:20 +0530

    Add valid-anagram.cpp

---
+++ valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dic1 = {} 
+        dic2 = {}
+        for i in s:
+            if i in dic1:
+                dic1[i]+=1
+            else:
+                dic1[i]=1
+        for i in t:
+            if i in dic2:
+                dic2[i]+=1
+            else:
+                dic2[i]=1
+        if dic1==dic2:
+            return True
+        return False
